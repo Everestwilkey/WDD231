56e81983f7571feccb5e6889aa4217242f8175cc
Everest Wilkey <131012192+Everestwilkey@users.noreply.github.com>
2024-11-14 13:23:37 -0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: "index.html",
-        conditions: "conditions.html",
+        main: resolve(__dirname, "src/index.html"),
+        conditions: resolve(__dirname, "src/conditions.html"),
       },
     },
   },
