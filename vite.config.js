fb24b916e91d2e85423c6f426d3650ace4389220
Everest Wilkey <131012192+Everestwilkey@users.noreply.github.com>
2024-12-07 19:15:05 -0700

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
+  root: "src", // Specifies that "src" is the root folder
   build: {
-    outDir: "../dist",
-    emptyOutDir: true,
+    outDir: "../dist", // Outputs files to "dist" in the project root
+    emptyOutDir: true, // Clears the "dist" folder before building
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        conditions: resolve(__dirname, "src/conditions.html")
-      }
-    }
-  }
+        main: resolve(__dirname, "src/index.html"), // Main entry point
+        conditions: resolve(__dirname, "src/conditions.html"), // Additional page
+        visitorCenter: resolve(__dirname, "src/visitor-center.html"), // Add any additional pages here
+      },
+    },
+  },
 });
